f0652dd8ae388d6ee7d90b348bb878d24f1ae834
kazushi67 <kazushifujimoto2@tier4.jp>
2026-01-06 13:58:30 +0900

    style: add todo comments
    Signed-off-by: kazushi67 <kazushifujimoto2@tier4.jp>

--- carla_agent_controller/node.py
+++ carla_agent_controller/node.py
@@ -22,6 +22,9 @@ from autoware_perception_msgs.msg import PredictedObjects
 
 
 class CarlaEgoFollower(Node):
+    """
+    to do: add documentation
+    """
     def __init__(self, host="127.0.0.1", port=2000, hz=20.0):
         super().__init__("carla_agent_controller")
         self.get_logger().info("launch carla_agent_controller")
